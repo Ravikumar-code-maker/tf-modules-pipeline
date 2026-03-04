7af210893c6919e155ce40dfc47f0b32a2f86313
Ravikumar-code-maker <ravikumarchinthapally@gmail.com>
2026-03-04 19:30:27 +0530

    Update main.tf

---
+++ gcp-infra-project/modules/network/main.tf
@@ -0,0 +1,29 @@
+resource "google_compute_network" "vpc" {
+  name                    = "${var.emv_name}-vpc"
+  auto_create_subnetworks = false
+  project                 = var.project_id
+}
+
+resource "google_compute_subnetwork" "subnet" {
+  name          = "${var.env_name}-subnet"
+  ip_cidr-range = var.subnet_cidr
+  network       = google_compute_network.vpc.id
+  region        = var.region
+}
+
+# Apply firewall rules
+resource "google_compute_firewall" "firewall" {
+  for_each = { for r in var.firewall_rules : r.name => r  }
+  name  = each.value.name
+  network = google_compute_network.vpc.name
+
+  dynamic "allow" {
+    for_each = each.value.allow
+    content {
+      protocol = allow.value.protocol
+      ports    = allow.value.ports
+    }
+  }
+  source_ranges = each.value.source_ranges
+}
+ 
