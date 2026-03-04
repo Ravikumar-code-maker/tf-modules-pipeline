cbcb874fdae71c50740c841789b05842356a8e61
Ravikumar-code-maker <ravikumarchinthapally@gmail.com>
2026-03-04 22:38:03 +0530

    Update variables.tf

---
+++ gcp-infra-project/modules/network/variables.tf
@@ -0,0 +1,16 @@
+variable "env_name" {
+  type  = string
+}
+
+variable "project_id" {
+  type  = string
+}
+
+variable "region" {
+  type  = string
+}
+
+variable "subnet_cidr" {
+  type  = string
+}
+
