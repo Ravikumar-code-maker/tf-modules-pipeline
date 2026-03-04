7a3eb8991d2babb4d29ebb7b2df0a2f21c753d36
Ravikumar-code-maker <ravikumarchinthapally@gmail.com>
2026-03-05 01:45:41 +0530

    Update main.tf

---
+++ gcp-infra-project/modules/bigquery/main.tf
@@ -0,0 +1,26 @@
+resource "google_bigquery-dataset" "dataset" {
+  dataset_id   = "${var.env_name}_dataset"
+  project      = var.project_id
+  location     = var.location
+}
+
+resource "google_bigquery_table" "table" {
+  for_each = toset(var.tables)
+
+  table_id = each.key\
+  dataset_id = google_bigquery_dataset.dataset.dataset_id
+  project    = var.project_id
+
+  schema = jsonencode ([
+    {
+     name = "id",
+     type = "STRING",
+     mode = "REQUIRED"
+    }
+    {
+     name = "value",
+     type = "STRING",
+     mode = "NULLABLE"
+    }
+  ])
+}
